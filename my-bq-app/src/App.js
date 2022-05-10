6c10a511168e1edfcb5cdb06f03aee86074f50a0
sarai gonzalez <saraiglezyglez@gmail.com>
2022-05-09 20:41:24 -0500

    creation of jsx and style of home page of HU1

--- my-bq-app/src/App.js
+++ my-bq-app/src/App.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react';
+import mainLogo from'./assets/wallpaper.png';
+import oliveLogo from './assets/olive.png';
+import burgerLogo from './assets/burgerNoRadius.png';
 import { Routes, Route, /* useNavigate*/ } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BreakfastPage from './pages/BreakfastPage';
@@ -7,8 +10,23 @@ import NavMenus from './components/NavMenus';
 import './styles/App.css';
 
 function App() {
-
-  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <section className="App">
+      <img className='mainLogo' src={mainLogo} alt="wallpaper-img" />
+      <div className='mainFrame'>
+        <div className='secondFrame'>
+          <div className='restauBrand'>
+            <img src={oliveLogo} alt="olive-img" />
+            <label className="home-title">Burger Queen</label>
+          </div>
+          <div className="home-slogan-brand">
+                <label className="home-slogan">Let's eat this <br></br>burger together!</label>
+                <img src={burgerLogo} alt="burger-img" />
+          </div>
+        </div>
+      </div>
+    </section>
+ /* const [isVisible, setIsVisible] = useState(false);
 
   const onPress = () => {
     setIsVisible(!isVisible);
@@ -23,8 +41,8 @@ function App() {
         <Route path= "breakfast-menu" element = {<BreakfastPage />} />
         <Route path= "lunch-menu" element = {<LunchPage />} />
       </Routes>
-    </div>
-  );
-}
+    </div> */
+  ); 
+} 
 
 export default App;
---
+++ my-bq-app/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import MainSection from "../components/MainSection";
+
+const HomePage = ({}) => {
+    return (
+      <>
+            <MainSection 
+              
+              height={300}
+              width={300}
+              title={"BurgerQueen"}
+              image1={"./assets/olive.png"}
+              slogan={"Let's eat this burger together!"}
+              image2={"./assets/burger.png"}
+            />
+            </>
+    )
+  }
+  
+  export default HomePage
